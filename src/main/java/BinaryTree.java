bf4b8da66112044162e39857e9095b857a0961f2
Vadim <vadikn1994@gmail.com>
2018-09-30 21:34:18 +0300

    First commit

---
+++ src/main/java/BinaryTree.java
@@ -0,0 +1,23 @@
+public class BinaryTree<T> {
+    class Node{
+        private int key;
+        private T value;
+        private Node left = null, right = null;
+        Node(T element){
+            value = element;
+        }
+    }
+    BinaryTree(){
+
+    }
+
+    private Node tree = null;
+    public void add(T element){
+        Node node = ;
+        while
+    }
+
+    public static void main(String[] args){
+
+    }
+}
